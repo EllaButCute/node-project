7ea55b0d317111cd1633d3c7d5596d700cacc3a9
Ella <30332105+ERROR200@users.noreply.github.com>
2020-04-23 15:43:08 +1000

    feat(utils): add log and add functions

--- app/utils.ts
+++ app/utils.ts
@@ -1,3 +1,9 @@
 export const capitalise = (...words: string[]) => {
     return words.map(word => word.toUpperCase()).join('-');
 }
+export const log = (message: string) => {
+    console.info(message)
+}
+export const add = (firstValue: number, secondValue?: number) => {
+    return firstValue + (secondValue ?? 20)
+}
